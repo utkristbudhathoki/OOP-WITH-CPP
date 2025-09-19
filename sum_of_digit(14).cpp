3fecc74a7550657c56ae36a3eb0bfddbb470d662
utkristbudhathoki <utkristbudhathoki19@gmail.com>
2025-09-19 21:57:01 +0545

    sum of dgit

---
+++ sum_of_digit(14).cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main(){
+int i,sum=0,n;
+cout<<"\nEnter the number:";
+cin>>n;
+while(n>0){
+	sum = sum+n%10;
+    n = n/10;
+}
+cout<<"sum:"<<sum;
+return 0;
+}

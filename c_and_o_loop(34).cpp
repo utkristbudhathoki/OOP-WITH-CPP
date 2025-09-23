a042a9b8991539c44b5c6821e8aab388c485150f
utkristbudhathoki <utkristbudhathoki19@gmail.com>
2025-09-23 21:01:09 +0545

    initial commit

---
+++ c_and_o_loop(34).cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+class oneten 
+{
+	public:
+	void putdata()
+	{
+		int i ;
+		 for(i=1;i<=10;i++)
+		 cout<<"\n"<<i;
+	 }
+ };
+int main(){
+	oneten aa;
+	aa.putdata();
+	return 0;
+}
